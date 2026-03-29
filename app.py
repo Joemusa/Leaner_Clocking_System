b00cec6f8b52283f653fe3dd805048953dec3c74
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-03-29 07:01:12 +0200

    Reduce figure width in plot to 3 inches

--- app.py
+++ app.py
@@ -355,7 +355,7 @@ with tab1:
                 "font.size": 8  # smaller consistent font
             })
     
-            fig, ax = plt.subplots(figsize=(6,4))
+            fig, ax = plt.subplots(figsize=(3,4))
     
             pivot.plot(kind="bar", stacked=True, ax=ax)
     
