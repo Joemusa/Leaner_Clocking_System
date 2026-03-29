57e3fe4ea9729605ca65f2129b156f967c847004
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-03-29 07:06:41 +0200

    Increase figure width from 10 to 12 in plot

--- app.py
+++ app.py
@@ -355,7 +355,7 @@ with tab1:
                 "font.size": 8  # smaller consistent font
             })
     
-            fig, ax = plt.subplots(figsize=(10,4))
+            fig, ax = plt.subplots(figsize=(12,4))
     
             pivot.plot(kind="bar", stacked=True, ax=ax)
     
