120a39ad0d5110b2e15d93d4a3c73278a95a042f
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-05-16 06:18:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -777,7 +777,7 @@ with tab4:
     # -----------------------------
     # PRESENT LEARNERS (IN ONLY)
     # -----------------------------
-    k1, k2, k3 = st.columns(3)
+     k1, k2, k3 = st.columns(3)
     
         
         with k1:
