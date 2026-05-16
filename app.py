ab3f141718be50728c7be652ff51c6defcdc6673
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-05-16 06:03:06 +0200

    Adjust layout by removing a column and commenting metric
    Removed a column from the layout and commented out the present metric.

--- app.py
+++ app.py
@@ -795,9 +795,9 @@ with tab4:
     
     st.info(f"Date: {selected_date.date()}")
     
-    col1, col2, col3 = st.columns(3)
+    col1, col3 = st.columns(2)
     col1.metric("Registered", total_registered)
-    col2.metric("Present", total_present)
+    #col2.metric("Present", total_present)
     col3.metric("Absent", total_absent)
     
     # -----------------------------
